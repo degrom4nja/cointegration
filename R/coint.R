150839e295d096b2fd0e3a71fc8fe87a9c31f35c
inoopus <inoopus0@gmail.com>
2022-04-06 23:33:37 +0900

    first commit

---
+++ R/coint.R
@@ -0,0 +1,355 @@
+egc_ersd_qtab <- structure(list(quantile = c(NA, 1e-04, 0.01, 0.025, 0.05, 0.1,
+0.2, 0.5, 0.8, 0.9, 0.95, 0.975, 0.99, 0.999, 0.9999), `25` = c(25,
+-5.10048956847502, -3.30687485693397, -2.90155974190788, -2.59129558300045,
+-2.27276240072175, -1.92213650552829, -1.34798299473902, -0.838329861629613,
+-0.509185593064544, -0.195300939641466, 0.108914109087463, 0.482149591268087,
+1.32699563522915, 2.10728039324104), `50` = c(50, -4.6199438788185,
+-3.21676499222889, -2.8686006810382, -2.59803884713455, -2.29576353217148,
+-1.94583374315336, -1.33829556692924, -0.781175098555971, -0.397080658949842,
+-0.0296455279811186, 0.32631220386273, 0.734690136808975, 1.62622811094037,
+2.24605507511686), `100` = c(100, -4.51297528529818, -3.17156669441703,
+-2.83736242414913, -2.55094926095896, -2.24977219003744, -1.88595137575393,
+-1.24474595982246, -0.638191937730234, -0.233009600573567, 0.160302569814843,
+0.51339966804765, 0.951450453947234, 1.81236847378032, 2.69868923028809
+), `250` = c(250, -4.37525803985682, -3.11239840487866, -2.80614575845577,
+-2.53816683428338, -2.20592284886742, -1.83484806078488, -1.1468850571759,
+-0.491236431122179, -0.0764269107363089, 0.312863660226734, 0.666606184291284,
+1.10400057132307, 1.9447657288027, 2.77038477905867), `500` = c(500,
+-4.25804077548967, -3.12780400002265, -2.81162151984338, -2.52181493428759,
+-2.18877158176733, -1.7876628512075, -1.08682320198154, -0.405262455957855,
+0.0217186030751472, 0.389864188676898, 0.738630355309322, 1.15496529052512,
+2.05416971254848, 2.90495065530857), `750` = c(750, -4.36147063522307,
+-3.14245449845762, -2.78950724295031, -2.48675051561575, -2.1653912230568,
+-1.78073016343705, -1.06913732108122, -0.381437995991238, 0.0484265194036463,
+0.434833031029738, 0.77225944105774, 1.18255474811806, 1.99386245001663,
+2.80529292874148), `1000` = c(1000, -4.50739170629214, -3.13712650619975,
+-2.80431314786342, -2.50954785628484, -2.18074731078809, -1.78467925508109,
+-1.07259971623705, -0.382209469861783, 0.0576012359080366, 0.435548812016741,
+0.800719533205871, 1.18775043814421, 2.08391466082401, 2.78987232526501
+), `1250` = c(1250, -4.52635229954304, -3.12054809254334, -2.8022823402926,
+-2.5028174903535, -2.16809873100608, -1.77226225298197, -1.06144666113106,
+-0.368677919111695, 0.0550234663325856, 0.445648528283737, 0.807052347089671,
+1.19924309600821, 1.96541372814745, 2.62092875436772), `2500` = c(2500,
+-4.47168620669166, -3.13545292848152, -2.78588889223927, -2.4880731613059,
+-2.15658084744067, -1.77365567712487, -1.04468871221604, -0.353635563847591,
+0.0725063965562995, 0.464447482050564, 0.817423489553938, 1.21452650295202,
+2.16613650569432, 2.8166674020312)), .Names = c("quantile", "25",
+"50", "100", "250", "500", "750", "1000", "1250", "2500"), row.names = c("",
+"0.01%", "1%", "2.5%", "5%", "10%", "20%", "50%", "80%", "90%",
+"95%", "97.5%", "99%", "99.9%", "99.99%"), class = "data.frame")
+
+egc_jot_qtab <- structure(list(quantile = c(NA, 1e-04, 0.01, 0.025, 0.05, 0.1,
+0.2, 0.5, 0.8, 0.9, 0.95, 0.975, 0.99, 0.999, 0.9999), `25` = c(25,
+-43.6039139855407, -28.6829709794164, -25.6300537603927, -23.3079359185719,
+-20.643739189464, -17.7884740594005, -13.1348239053396, -9.58693660153554,
+-8.05909112818845, -7.01406411497141, -6.19569951461884, -5.35591475011705,
+-4.08260696696836, -3.31049871144232), `50` = c(50, -38.4752049971958,
+-26.8080988779882, -23.9437700481207, -21.5574845593767, -19.0774684501909,
+-16.5045018454215, -12.183631307656, -8.84373681572512, -7.46100621867351,
+-6.47530477837974, -5.69218935468569, -4.89007873490613, -3.66579155674075,
+-2.7324831220996), `100` = c(100, -40.2124175203202, -25.5893372632967,
+-23.0323211816574, -20.810618288626, -18.5040229104433, -15.9183860115843,
+-11.74611718597, -8.55756459180621, -7.1880271146337, -6.1612913901399,
+-5.4259638635224, -4.66717848573724, -3.64213982185279, -2.82147037506989
+), `250` = c(250, -36.8363870121499, -25.503723090232, -22.7913067921628,
+-20.5647324983675, -18.3227705275422, -15.7148313468566, -11.5691663072761,
+-8.37606828061385, -7.01749168898447, -6.07378903078064, -5.33387231670868,
+-4.55762374951882, -3.37367431197166, -2.71177217407318), `500` = c(500,
+-35.8931079990305, -25.5067935105137, -22.6971799724303, -20.4943323346414,
+-18.0772809282935, -15.5533042944476, -11.4923511862326, -8.32760076448148,
+-7.00568646305856, -6.07714694733917, -5.32811847017675, -4.58102351459653,
+-3.25159378652592, -2.65054723217171), `750` = c(750, -37.745032170643,
+-25.0822599618117, -22.4732901668475, -20.3703679268076, -18.0870793164142,
+-15.5682318967508, -11.4780249865322, -8.33286518403125, -7.00996697530282,
+-6.04396418081035, -5.30102486235099, -4.57043493336911, -3.35455895999861,
+-2.73356562079151), `1000` = c(1000, -36.2344573655043, -25.1038801150724,
+-22.3998867232976, -20.206581784596, -17.9432295463599, -15.4644455837395,
+-11.4531807056099, -8.33200153188878, -6.98751115066501, -5.99846251864034,
+-5.30359410405053, -4.55912626375865, -3.33973427265424, -2.69388481757016
+), `1250` = c(1250, -36.422058957083, -25.1248808353849, -22.4460546913756,
+-20.3446739203963, -18.0567540031894, -15.5546815514663, -11.4659570172447,
+-8.29301400579434, -6.94723871532745, -5.97833367858986, -5.20389564066867,
+-4.44983350810362, -3.32349110694732, -2.64683960242408), `2500` = c(2500,
+-35.6379697758194, -25.2733069974919, -22.5247104652215, -20.2349350303678,
+-18.018940187706, -15.4994530355328, -11.4738335040917, -8.29267196408465,
+-6.97256431918771, -6.01373746763148, -5.29545588131238, -4.58138773087048,
+-3.30109427170537, -2.42493858524213)), .Names = c("quantile",
+"25", "50", "100", "250", "500", "750", "1000", "1250", "2500"
+), row.names = c("", "0.01%", "1%", "2.5%", "5%", "10%", "20%",
+"50%", "80%", "90%", "95%", "97.5%", "99%", "99.9%", "99.99%"
+), class = "data.frame")
+
+quantile_table_interpolate <- function (qtab, sample_size, stat, stop.on.na=FALSE)
+{
+  n <- nrow(qtab)
+  i <- findInterval(sample_size, qtab[1,2:ncol(qtab)])+1
+  if (i == 1) {
+    parent_name <- as.character(sys.call(-1)[[1]])
+    if (stop.on.na) {
+      stop (parent_name," requires a minimum of ", qtab[1,2], " observations.")
+    } else {
+      warning (parent_name, " requires a minimum of ", qtab[1,2], " observations.")
+      return(NA)
+    }
+  }
+  y1 <- approx(qtab[2:n, i], qtab[2:n, 1], stat, rule=2)$y
+  if (i < ncol(qtab)) {
+    y2 <- approx(qtab[2:n, i+1], qtab[2:n, 1], stat, rule=2)$y
+    n1 <- qtab[1,i]
+    n2 <- qtab[1,i+1]
+    y <- y1 * (n2 - sample_size) / (n2 - n1) + y2 * (sample_size - n1)/(n2 - n1)
+  } else {
+    y <- y1
+  }
+  y
+}
+
+cvglmnet <- function(formula, alpha = c(0, 1, 0.5))
+{
+  return(cv.glmnet(x=model.matrix(formula), y=model.response(model.frame(formula)), alpha = alpha))
+}
+
+coint <- function(x, y, method=c("df", "po", "jo", "jovec"))
+{
+  if(method == "df")
+    co.df(x, y)
+  else if(method == "po")
+    co.po(x, y)
+  else if(method == "jo")
+    co.jo(x, y)
+  else if(method == "jovec")
+    co.jovec(x, y)
+}
+
+adf <- function(x)
+{
+  ers <- ur.ers(x, model = "trend", lag.max = trunc(4*(length(x)/100)^0.25))
+  p.value <- quantile_table_interpolate(egc_ersd_qtab, length(x), ers@teststat[1])
+  p.value
+}
+
+co.po <- function(x, y)
+{
+  corrcoef <- cor(x, y)
+  t <- 0.0
+  data <- cbind(y, x)
+  model <- "constant"
+  po <- ca.po(data, demean=model)
+  a <- po@testreg$coefficients[1,1]
+  b <- po@testreg$coefficients[2,1]
+  a.p <- po@testreg$coefficients[1,4]
+  if(a.p > 0.05)
+  {
+    model <- "none"
+    po <- ca.po(data, demean=model)
+    a <- 0
+    b <- po@testreg$coefficients[1,1]
+  }
+
+  stat <- po@teststat[1]
+  cval <- po@cval[1,2]
+  residuals <- po@res[,1]
+  PVAL <- -stat
+  integrated <- abs(corrcoef) >= 0.9 && stat > cval
+
+  structure(list(index=1:length(x), model=model, X=x, Y=y, trend=t, alpha=a, beta=b, resid=residuals, sd=sd(residuals), mean=mean(residuals), max=max(residuals), min=min(residuals), corr=corrcoef, p.value=PVAL, cointegrated=integrated, class="coint"))
+}
+
+co.df <- function(x, y)
+{
+  corrcoef <- cor(x, y)
+  #model <- cvglmnet(y ~ x, alpha = 1)
+  #coef <- coef(model, s = "lambda.min")
+  #a <- coef[1]
+  #b <- coef[3]
+  #residuals <- y - (a + b * x)
+
+  m <- "constant"
+  model <- lm(y ~ x)
+  a <- model$coefficients[1]
+  b <- model$coefficients[2]
+  residuals <- model$residuals
+  ers <- ur.ers(residuals, model = "constant", lag.max = trunc(4*(length(residuals)/100)^0.25))
+  stat <- ers@teststat[1]
+  cval <- ers@cval[1,2]
+  PVAL <- quantile_table_interpolate(egc_ersd_qtab, length(residuals), stat)
+  integrated <- (corrcoef > 0.8 || corrcoef < -0.8) && PVAL < 0.05
+
+  structure(list(index=1:length(x), model=m, X=x, Y=y, alpha=a, beta=b, resid=residuals, sd=sd(residuals), mean=mean(residuals), max=max(residuals), min=min(residuals), corr=corrcoef, p.value=PVAL, cointegrated=integrated, class="coint"))
+}
+
+co.jovec <- function(x, y)
+{
+  corrcoef<-cor(x,y)
+  t <- 0.0
+  rnk <- 0
+  jo<-ca.jo(cbind(y,x), ecdet="const")
+  stat0 <- jo@teststat[2]
+  stat1 <- jo@teststat[1]
+  cval0 <- jo@cval[2,2]
+  cval1 <- jo@cval[1,2]
+  if(stat0 > cval0)
+    rnk <- 1
+
+  if(stat1 > cval1)
+    rnk <- 2
+
+  rank <- rnk
+  if(rank == 0)
+    rank = 1
+  var <- vec2var(jo, r = rank)
+  a <- var$deterministic[1]
+  by1 <- var$A$A1[1,1]
+  bx1 <- var$A$A1[1,2]
+  by2 <- var$A$A2[1,1]
+  bx2 <- var$A$A2[1,2]
+  y.lag1 <- lag(y,n=1)
+  y.lag2 <- lag(y,n=2)
+  x.lag1 <- lag(x,n=1)
+  x.lag2 <- lag(x,n=2)
+  residuals<-resid(var)[,1]
+
+  PVAL <- -stat0 #quantile_table_interpolate(egc_jot_qtab, length(residuals), -stat)
+  integrated <- stat0 > cval0
+  structure(list(index=1:length(x), model="constant", rank=rnk, X=x, Y=y, x1=x.lag1, y1=y.lag1, x2=x.lag2, y2=y.lag2, alpha=a, beta.y1=by1, beta.x1=bx1, beta.y2=by2, beta.x2=bx2, resid=residuals, sd=sd(residuals), mean=mean(residuals), corr=corrcoef, p.value=PVAL, cointegrated=integrated, class="coint"))
+}
+
+co.jo <- function(x, y)
+{
+  corrcoef<-cor(x,y)
+  t <- 0.0
+  jo<-ca.jo(cbind(y,x), type = "trace", ecdet="const")
+  stat0 <- jo@teststat[2]
+  stat1 <- jo@teststat[1]
+  cval0 <- jo@cval[2,2]
+  cval1 <- jo@cval[1,2]
+  rnk <- 0
+  if(stat0 > cval0)
+    rnk <- 1
+  if(stat1 > cval1)
+    rnk <- 2
+
+  a <- -jo@V[3,1]
+  b <- -jo@V[2,1]
+  residuals <- y - (a + b * x)
+
+  PVAL <- -stat0 #quantile_table_interpolate(egc_jot_qtab, length(residuals), -stat)
+  integrated <- (corrcoef > 0.7 || corrcoef < -0.7) && rnk > 0
+  structure(list(index=1:length(x), model="constant", rank=rnk, X=x, Y=y, trend=t,  alpha=a, beta=b, resid=residuals, sd=sd(residuals), mean=mean(residuals), max=max(residuals), min=min(residuals), corr=corrcoef, p.value=PVAL, cointegrated=integrated, class="coint"))
+}
+
+jo.plot <- function(C)
+{
+  grid.newpage()
+  pushViewport(viewport(layout=grid.layout(10,1)))
+
+  series_names <- c("x", "y")
+  y.df <- data.frame(Date=C$index, Value=as.numeric(C$Y), Facet=series_names[2], Series="Price")
+  R.df <- data.frame(Date=1:length(C$resid), Value=C$resid, Facet="R", Series="Residuals")
+
+  x.scaled <- as.numeric(C$alpha + C$beta.y1 * C$y1 + C$beta.x1 * C$x1 + C$beta.y2 * C$y2 + C$beta.x2 * C$x2)
+  s1scaled_desc <- sprintf("%.3f", C$alpha)
+  x.scaled.df <- data.frame(Date=C$index, Value=x.scaled, Facet=s1scaled_desc, Series="Price")
+  df <- rbind(x.scaled.df, y.df)
+  p1 <- ggplot(df, aes(x=Date, y=Value, colour=Facet)) + geom_line() +
+    ylab("Price") + xlab("") + theme(legend.position="top") +
+    scale_colour_discrete(name="") + ggtitle ("Price Series")
+
+  if(!C$cointegrated)
+  {
+    x <- min(df$Date)
+    ymin <- min(df$Value)
+    ymax <- max(df$Value)
+    y <- ymin + 0.96 * (ymax - ymin)
+    p1 <- p1 + annotate("text", x=x, y=y, label="Not cointegrated", colour="red", hjust=0, size=3)
+  }
+
+  print(p1, vp=viewport(layout.pos.row=2:5, layout.pos.col=1))
+
+  hlines = data.frame(Value=c(C$mean + 3 * C$sd, C$mean + 2 * C$sd, C$mean + -2 * C$sd, C$mean + -3 * C$sd), Facet=c("three", "two", "two", "three"))
+  p2 <- ggplot(R.df, aes(x=Date, y=Value)) + geom_line() +
+    ggtitle ("Residual Series") + ylab("Differential") + xlab("") +
+    geom_hline(data=hlines, aes(yintercept=Value, colour=Facet)) +
+    guides(colour=FALSE)
+
+  print(p2, vp=viewport(layout.pos.row=6:9, layout.pos.col=1))
+}
+
+jo.test <- function(C, x, y)
+{
+  grid.newpage()
+  pushViewport(viewport(layout=grid.layout(10,1)))
+
+  series_names <- c("x", "y")
+  index <- 1:length(x)
+  y.df <- data.frame(Date=index, Value=as.numeric(y), Facet=series_names[2], Series="Price")
+
+  y1<-lag(y, n=1)
+  y2<-lag(y, n=2)
+  x1<-lag(x, n=1)
+  x2<-lag(x, n=2)
+  scale <- C$alpha + C$beta.y1 * y1 + C$beta.x1 * x1 + C$beta.y2 * y2 + C$beta.x2 * x2
+  resid <- as.numeric(y - scale)
+  R.df <- data.frame(Date=index, Value=resid, Facet="R", Series="Residuals")
+
+  x.scaled <- as.numeric(scale)
+  s1scaled_desc <- sprintf("%.2f", C$alpha)
+  x.scaled.df <- data.frame(Date=index, Value=x.scaled, Facet=s1scaled_desc, Series="Price")
+  df <- rbind(x.scaled.df, y.df)
+  p1 <- ggplot(df, aes(x=Date, y=Value, colour=Facet)) + geom_line() +
+    ylab("Price") + xlab("") + theme(legend.position="top") +
+    scale_colour_discrete(name="") + ggtitle ("Price Series")
+
+  print(p1, vp=viewport(layout.pos.row=2:5, layout.pos.col=1))
+
+  hlines = data.frame(Value=c(C$mean + 4 * C$sd, C$mean + 3 * C$sd, C$mean + -3 * C$sd, C$mean + -4 * C$sd), Facet=c("three", "two", "two", "three"))
+  p2 <- ggplot(R.df, aes(x=Date, y=Value)) + geom_line() +
+    ggtitle ("Residual Series") + ylab("Differential") + xlab("") +
+    geom_hline(data=hlines, aes(yintercept=Value, colour=Facet)) +
+    guides(colour=FALSE)
+
+  print(p2, vp=viewport(layout.pos.row=6:9, layout.pos.col=1))
+}
+
+sign_string <- function (x)
+{
+  ifelse (x < 0, "-", "+")
+}
+
+co.plot <- function(C)
+{
+  grid.newpage()
+  pushViewport(viewport(layout=grid.layout(10,1)))
+
+  series_names <- c("x", "y")
+  y.df <- data.frame(Date=C$index, Value=as.numeric(C$Y), Facet=series_names[2], Series="Price")
+
+  R.df <- data.frame(Date=C$index, Value=C$resid, Facet="R", Series="Residuals")
+
+  x.scaled <- as.numeric(C$alpha + C$beta * C$X)
+  s1scaled_desc <- sprintf("%.3f %s %.3f * %s", C$alpha, sign_string(C$beta), abs(C$beta), series_names[1])
+  x.scaled.df <- data.frame(Date=C$index, Value=x.scaled, Facet=s1scaled_desc, Series="Price")
+  df <- rbind(x.scaled.df, y.df)
+  p1 <- ggplot(df, aes(x=Date, y=Value, colour=Facet)) + geom_line() +
+    ylab("Price") + xlab("") + theme(legend.position="top") +
+    scale_colour_discrete(name="") + ggtitle ("Price Series")
+
+  if(!C$cointegrated)
+  {
+    x <- min(df$Date)
+    ymin <- min(df$Value)
+    ymax <- max(df$Value)
+    y <- ymin + 0.96 * (ymax - ymin)
+    p1 <- p1 + annotate("text", x=x, y=y, label="Not cointegrated", colour="red", hjust=0, size=3)
+  }
+
+  print(p1, vp=viewport(layout.pos.row=2:5, layout.pos.col=1))
+
+  hlines = data.frame(Value=c(C$mean + 3 * C$sd, C$mean + C$sd, C$mean + -C$sd, C$mean + -3 * C$sd), Facet=c("three", "one", "one", "three"))
+  p2 <- ggplot(R.df, aes(x=Date, y=Value)) + geom_line() +
+    ggtitle ("Residual Series") + ylab("Differential") + xlab("") +
+    geom_hline(data=hlines, aes(yintercept=Value, colour=Facet)) +
+    guides(colour=FALSE)
+
+  print(p2, vp=viewport(layout.pos.row=6:9, layout.pos.col=1))
+}
